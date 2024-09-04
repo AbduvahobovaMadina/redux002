29fc44c323cf6af2b6c709feae4d34be33d58a40
AbduvahobovaMadina <mailto:madinaabduvahobova565@gmail.com>
2024-09-04 11:21:55 +0500

    finish

--- src/companents/blogs/Blogs.jsx
+++ src/companents/blogs/Blogs.jsx
@@ -31,16 +31,16 @@ const Blogs = ({ data }) => {
     <div key={blog._id} className="w-56 flex gap-2 flex-col rounded-xl p-5 border ">
       <h3 className="text-xl font-sans">{blog.title}</h3>
       <p className="text-xs font-sans">{blog.desc}</p>
-      <Button onClick={() => handlDelete(blog._id)}>Delete</Button>
+      <Button className="bg-red-400 text-white" onClick={() => handlDelete(blog._id)}>Delete</Button>
     </div>
   ));
  
   return (
     <div className="container mx-auto mt-8 mb-10">
-      <Button onClick={() => setShow(true)} className="mb-6 ">
+      <Button onClick={() => setShow(true)} className="mb-6  bg-green-400 text-white ">
         <p className="font-bold">Add</p>
       </Button>
-      <div className="grid gap-4   grid-cols-1  lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2">
+      <div className="grid gap-4 grid-cols-1  lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2">
         {items}
       </div>
       <Add show={show} setShow={setShow} handleCreate={handleCreate} />
