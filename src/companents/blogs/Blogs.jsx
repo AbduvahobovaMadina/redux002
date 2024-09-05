af1b266fd0f228265326a8dc90263f43fb93d1a1
AbduvahobovaMadina <mailto:madinaabduvahobova565@gmail.com>
2024-09-05 23:17:14 +0500

    /

--- src/companents/blogs/Blogs.jsx
+++ src/companents/blogs/Blogs.jsx
@@ -16,6 +16,7 @@ const Blogs = ({ data }) => {
       setReload((p) => !p);
     });
   };
+
   const handleCreate = (values) => {
     let blog = {
       title: values.title,
@@ -31,6 +32,7 @@ const Blogs = ({ data }) => {
     <div key={blog._id} className="w-56 flex gap-2 flex-col rounded-xl p-5 border ">
       <h3 className="text-xl font-sans">{blog.title}</h3>
       <p className="text-xs font-sans">{blog.desc}</p>
+      <p>Created by: {blog.userId.fname}</p>
       <Button className="bg-red-400 text-white" onClick={() => handlDelete(blog._id)}>Delete</Button>
     </div>
   ));
